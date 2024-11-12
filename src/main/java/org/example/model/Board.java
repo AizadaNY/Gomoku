faf61476044463cd83b32d7c5206e81683fb6d4e
AizadaNY <aizada.abdyrakhmanova@gmail.com>
2024-11-11 21:59:24 -0500

    new commit

---
+++ src/main/java/org/example/model/Board.java
@@ -0,0 +1,14 @@
+package org.example.model;
+
+public class Board {
+
+    private int row;
+    private int column;
+
+    public Board(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+
+}
---
+++ src/main/java/org/example/model/Player.java
@@ -0,0 +1,14 @@
+package org.example.model;
+
+public class Player {
+
+    private String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public void move(int xCoordinate,int yCoordinate){
+
+    }
+}
